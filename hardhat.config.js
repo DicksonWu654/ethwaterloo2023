dd259d6b316a486c5e10b7e41c6a18cb2d0bfacb
Dickson Wu <33645481+DicksonWu654@users.noreply.github.com>
2023-06-24 18:44:51 -0400

    Deployed contracts on chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-verify");
 require('dotenv').config()
 
 const POLYGON_ZK_EVM_TESTNET_URL = process.env.POLYGON_ZK_EVM_TESTNET_URL //quicknode
@@ -6,6 +7,12 @@ const POLYGON_MUMBIA_URL = process.env.POLYGON_MUMBIA_URL //quicknode
 const LINEA_URL = process.env.LINEA_URL //chainlist
 const GNOSIS_CHIADO_URL = process.env.GNOSIS_CHIADO_URL //chainlist
 const XDC_APOTHEM_URL = process.env.XDC_APOTHEM_URL //chainlist
+
+const POLYGON_ZK_EVM_ETHERSCAN_API_KEY = process.env.POLYGON_ZK_EVM_ETHERSCAN_API_KEY
+const POLYGON_MUMBIA_ETHERSCAN_API_KEY = process.env.POLYGON_MUMBIA_ETHERSCAN_API_KEY
+const LINEA_ETHERSCAN_API_KEY = process.env.LINEA_ETHERSCAN_API_KEY
+const GNOSIS_CHIADO_ETHERSCAN_API_KEY = process.env.GNOSIS_CHIADO_ETHERSCAN_API_KEY
+const XDC_APOTHEM_ETHERSCAN_API_KEY = process.env.XDC_APOTHEM_ETHERSCAN_API_KEY
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const XDC_PRIVATE_KEY = process.env.XDC_PRIVATE_KEY
@@ -17,7 +24,7 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 2 ** 32 - 1,
+        // runs: 2 ** 32 - 1,
       },
     },
   },
@@ -27,10 +34,13 @@ module.exports = {
       accounts: [`0x${PRIVATE_KEY}`],
       chainId: 1442,
     },
-    polygon_mumbai: {
+    polygon_mumbai: { // had a lot of problems with mumbia
       url: `${POLYGON_MUMBIA_URL}`,
       accounts: [`0x${PRIVATE_KEY}`],
       chainId: 80001,
+      // gasPrice: 10000000000,
+      gasMultiplier: 100,
+      // gas: 2000000,
     },
     linea: {
       url: `${LINEA_URL}`,
@@ -41,11 +51,61 @@ module.exports = {
       url: `${GNOSIS_CHIADO_URL}`,
       accounts: [`0x${PRIVATE_KEY}`],
       chainId: 10200,
+      gasPrice: 100,
     },
     xdc_apothem: {
       url: `${XDC_APOTHEM_URL}`,
       accounts: [`0x${XDC_PRIVATE_KEY}`],
       chainId: 51,
     }
+  },
+  etherscan: {
+    apiKey: {
+      polygon_zk_evm_testnet: `${POLYGON_ZK_EVM_ETHERSCAN_API_KEY}`,
+      polygon_mumbai: `${POLYGON_MUMBIA_ETHERSCAN_API_KEY}`,
+      linea: `${LINEA_ETHERSCAN_API_KEY}`,
+      gnosis_chiado: `${GNOSIS_CHIADO_ETHERSCAN_API_KEY}`,
+      xdc_apothem: `${XDC_APOTHEM_ETHERSCAN_API_KEY}`,
+    },
+    customChains: [
+      {
+        network: "polygon_zk_evm_testnet",
+        chainId: 1442,
+        urls: {
+          apiURL: "https://api-testnet-zkevm.polygonscan.com/api",
+          browserURL: "https://testnet-zkevm.polygonscan.com"
+        }
+      },
+      {
+        network: "polygon_mumbai",
+        chainId: 80001,
+        urls: {
+          apiURL: "https://api-testnet.polygonscan.com/api",
+          browserURL: "https://mumbai.polygonscan.com/"
+        }
+      },
+      {
+        network: "linea",
+        chainId: 59140,
+        urls: {
+          apiURL: "https://api-testnet.lineascan.build/api",
+        }
+      },
+      {
+        network: "gnosis_chiado",
+        chainId: 10200,
+        urls: {
+          apiURL: "https://blockscout.com/gnosis/chiado/api",
+        }
+      },
+      {
+        network: "xdc_apothem",
+        chainId: 51,
+        urls: {
+          apiURL: "https://xdc.blocksscan.io/api",
+        }
+      }
+    ]
   }
+
 };
--- scripts/deploy_main_and_interact.js
+++ scripts/deploy_main_and_interact.js
@@ -3,24 +3,31 @@ const { ethers } = require("hardhat");
 async function main() {
     const FriendFi_raw = await ethers.getContractFactory("FriendFi");
     const FriendFi = await FriendFi_raw.deploy();
+    await FriendFi.deployed();
 
     console.log("FriendFi deployed to:", FriendFi.address);
 
     const MockToken_raw = await ethers.getContractFactory("MockToken");
     const WETH = await MockToken_raw.deploy("Wrapped ETH", "WETH", 10000000000);
+    await WETH.deployed();
     const USDC = await MockToken_raw.deploy("USD Coin", "USDC", 10000000000);
+    await USDC.deployed();
 
     console.log("WETH deployed to:", WETH.address);
     console.log("USDC deployed to:", USDC.address);
 
-    FriendFi.initialize_user();
-    FriendFi.create_loan(
+    const initialize_tx_hash = await FriendFi.initialize_user();
+    await initialize_tx_hash.wait();
+    console.log("User initialized at txhash:", initialize_tx_hash.hash);
+
+    const loan_tx_hash = await FriendFi.create_loan(
         30,
         86400,
         86400,
         WETH.address,
         1000
     );
+    console.log("Loan created at txhash:", loan_tx_hash.hash);
 
 }
 
